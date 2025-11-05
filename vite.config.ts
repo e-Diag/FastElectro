ca0784d3afe350a8805526f80faf007722c7a17b
e-Diag <ertimofey2005@gmail.com>
2025-11-05 20:16:40 +0300

    Versel import

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
+  import { SpeedInsights } from "@vercel/speed-insights/next"
 
   export default defineConfig({
     plugins: [react()],
